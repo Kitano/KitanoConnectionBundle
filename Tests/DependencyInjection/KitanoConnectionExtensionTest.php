1d8c22bcc99a348340c0375ea20b8af33597a9f5
Thomas Tourlourat <thomas@tourlourat.com>
2013-02-19 18:18:42 +0100

    Refractor TU. Remove fixtures for configuration.

--- Tests/DependencyInjection/KitanoConnectionExtensionTest.php
+++ Tests/DependencyInjection/KitanoConnectionExtensionTest.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Kitano\ConnectionBundle\DependencyInjection\KitanoConnectionExtension;
 use Symfony\Component\DependencyInjection\Definition;
 
-abstract class KitanoConnectionExtensionTest extends \PHPUnit_Framework_TestCase
+class KitanoConnectionExtensionTest extends \PHPUnit_Framework_TestCase
 {
     private $container;
     private $extension;
@@ -16,6 +16,12 @@ abstract class KitanoConnectionExtensionTest extends \PHPUnit_Framework_TestCase
     {
         $this->container = new ContainerBuilder();
         $this->extension = new KitanoConnectionExtension();
+        
+        $this->container->setDefinition('doctrine.orm.entity_manager', new Definition('stdClass')); // w00t
+        $this->container->registerExtension($this->extension);
+        
+        $bundle = new KitanoConnectionBundle();
+        $bundle->build($this->container); // Attach all default factories
     }
 
     public function tearDown()
@@ -23,14 +29,27 @@ abstract class KitanoConnectionExtensionTest extends \PHPUnit_Framework_TestCase
         unset($this->container, $this->extension);
     }
     
-    abstract protected function loadFromFile(ContainerBuilder $container, $file);
-
     public function testCustomConnectionManagedClass()
     {
-        $container = $this->getContainer('persistence_custom', true);
-
-        $this->assertEquals($container->getParameter('kitano_connection.managed_class.connection'),
-            'My\Entity\Connection');
+        $config = array(
+            "kitano_connection" => array (
+                "persistence" => array (
+                    "type" => "custom",
+                    "managed_class" => array (
+                        "connection" => "My\Entity\Connection",
+                    ),
+                ),
+            ),
+        );
+        
+        //User define a custom repository
+        $this->container->setDefinition('kitano_connection.repository.connection', new Definition('My\CustomRepository'));
+        
+        
+        $this->extension->load($config, $this->container);
+        $this->container->compile();
+        
+        $this->assertEquals($this->container->getParameter('kitano_connection.managed_class.connection'), 'My\Entity\Connection');
     }
 
     /**
@@ -38,7 +57,16 @@ abstract class KitanoConnectionExtensionTest extends \PHPUnit_Framework_TestCase
      */
     public function testCustomPersistenceType()
     {
-        $container = $this->getContainer('persistence_custom', false);
+        $config = array(
+            "kitano_connection" => array (
+                "persistence" => array (
+                    "type" => "custom",
+                ),
+            ),
+        );
+        
+        $this->extension->load($config, $this->container);
+        $this->container->compile();
     }
     
     public function testOrmPeristenceDefaultManagedClass()
@@ -74,26 +102,5 @@ abstract class KitanoConnectionExtensionTest extends \PHPUnit_Framework_TestCase
         $this->container->compile();
         
         $this->assertEquals($this->container->getParameter('kitano_connection.managed_class.connection'), 'My\Entity\Connection');
-    }
-    
-    protected function getContainer($file, $createCustomRepository = true)
-    {
-        $container = new ContainerBuilder();
-
-        if ($createCustomRepository) {
-            $definition = new Definition('My\CustomRepository');
-            $container->setDefinition('kitano_connection.repository.connection', $definition);
-        }
-
-        $kitanoConnection = new KitanoConnectionExtension();
-        $container->registerExtension($kitanoConnection);
-
-        $bundle = new KitanoConnectionBundle();
-        $bundle->build($container); // Attach all default factories
-        $this->loadFromFile($container, $file);
-
-        $container->compile();
-
-        return $container;
     }
 }
